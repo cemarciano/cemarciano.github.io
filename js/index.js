8f4d9a18dbe721f90c9f8764bc36e8971d04ed56
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2018-02-10 01:09:24 -0200

    Replace static age with actual calculations

--- js/index.js
+++ js/index.js
@@ -7,4 +7,11 @@ $(document).ready(function(){
 	var provider = "poli.ufrj.br";
 	$(".email").html(username + "@" + provider);
 
+	// Calculates my age and adds it to my story:
+	var birth = "1995-08-05";
+	birth = new Date(birth);
+	var today = new Date();
+	var age = Math.floor((today-birth) / (365.25 * 24 * 60 * 60 * 1000));
+	$('#age').html(age);
+
 });
