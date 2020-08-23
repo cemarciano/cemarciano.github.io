e734bb2d36653cd204cb6d7c30c11b24e2aecb36
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2020-08-23 01:10:40 -0300

    Start adding PhD information

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 	// Puts e-mail content where it belongs (to avoid bots):
 	var username = "cemarciano";
-	var provider = "poli.ufrj.br";
+	var provider = "cs.toronto.edu";
 	$(".email").html(username + "@" + provider);
 
 	// Calculates my age and adds it to my story:
